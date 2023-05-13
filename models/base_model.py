0876d1cc9d8cefde32d066d62324642328cf90a4
Ampong Boadu <ampsboadu@gmail.com>
2023-05-13 15:04:34 +0000

    __str__ implemented to return string rep of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -45,3 +45,14 @@ class BaseModel():
         Updates updated_at to current datetime
         """
         self.updated_at = datetime.now()
+
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
